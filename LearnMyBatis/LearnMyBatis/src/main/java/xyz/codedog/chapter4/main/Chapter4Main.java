6358d87b5e985904fb7adf6deb5a7057d9be869d
admin-zhang <764018641qq.com>
2020-03-18 13:31:43 +0800

    ssm+redis框架整合

--- LearnMyBatis/LearnMyBatis/src/main/java/xyz/codedog/chapter4/main/Chapter4Main.java
+++ LearnMyBatis/LearnMyBatis/src/main/java/xyz/codedog/chapter4/main/Chapter4Main.java
@@ -3,7 +3,9 @@ package xyz.codedog.chapter4.main;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import xyz.codedog.chapter4.mapper.RoleMapper;
+import xyz.codedog.chapter4.mapper.UserMapper;
 import xyz.codedog.chapter4.pojo.Role;
+import xyz.codedog.chapter4.pojo.User;
 import xyz.codedog.chapter4.utils.SqlSessionFactoryUtils;
 
 
@@ -21,8 +23,13 @@ public class Chapter4Main {
         try {
             sqlSession = SqlSessionFactoryUtils.openSqlSession();
             RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             Role role = roleMapper.getRole(1L);
+            User user = userMapper.getUser(1L);
+            System.out.println("role==================");
             log.info(((Role) role).getRoleName());
+            System.out.println("user==================");
+            log.info(user.getSexEnum().getName());
             log.info(role.getNote());
         }finally {
             if (sqlSession != null){
---
+++ LearnMyBatis/LearnMyBatis/src/main/java/xyz/codedog/chapter4/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package xyz.codedog.chapter4.mapper;
+
+import xyz.codedog.chapter4.pojo.User;
+
+public interface UserMapper {
+    public User getUser(Long id);
+}
