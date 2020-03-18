4dbeb79c184ac9202f652d8e1f9cf2b2f5a95401
admin-zhang <764018641@qq.com>
2020-03-18 22:35:24 +0800

    ssm+redis框架整合

--- LearnMyBatis/LearnMyBatis/src/test/java/xyz/codedog/chapter3/Chapter3Main.java
+++ LearnMyBatis/LearnMyBatis/src/test/java/xyz/codedog/chapter3/Chapter3Main.java
@@ -1,13 +1,21 @@
-package xyz.codedog.chapter3.main;
+package xyz.codedog.chapter3;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import xyz.codedog.chapter3.mapper.RoleMapper;
 import xyz.codedog.chapter3.pojo.Role;
 import xyz.codedog.chapter3.utils.SqlSessionFactoryUtils;
 
+/**
+ * @ClassName Chapter3Main
+ * @Description TODO
+ * @Author panda
+ * @Date 2020/3/18 下午10:11
+ * @Version 1.0
+ **/
 public class Chapter3Main {
     public static void main(String[] args) {
-        Logger log = Logger.getLogger(Chapter3Main.class);
+        Logger log = Logger.getLogger(xyz.codedog.chapter3.Chapter3Main.class);
         SqlSession sqlSession = null;
         try {
             sqlSession = SqlSessionFactoryUtils.openSqlSession();
--- LearnMyBatis/LearnMyBatis/src/test/java/xyz/codedog/chapter4/Chapter4Main.java
+++ LearnMyBatis/LearnMyBatis/src/test/java/xyz/codedog/chapter4/Chapter4Main.java
@@ -1,36 +1,48 @@
-package xyz.codedog.chapter4.main;
+package xyz.codedog.chapter4;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
+import org.junit.Test;
 import xyz.codedog.chapter4.mapper.RoleMapper;
 import xyz.codedog.chapter4.mapper.UserMapper;
 import xyz.codedog.chapter4.pojo.Role;
 import xyz.codedog.chapter4.pojo.User;
 import xyz.codedog.chapter4.utils.SqlSessionFactoryUtils;
 
-
 /**
  * @ClassName Chapter4Main
  * @Description TODO
  * @Author panda
- * @Date 2020/3/17 下午7:09
+ * @Date 2020/3/18 下午10:11
  * @Version 1.0
  **/
 public class Chapter4Main {
-    public static void main(String[] args) {
-        Logger log = Logger.getLogger(Chapter4Main.class);
-        SqlSession sqlSession = null;
+
+    Logger log = Logger.getLogger(xyz.codedog.chapter4.Chapter4Main.class);
+    SqlSession sqlSession = null;
+
+    @Test
+    public void testRoleMapper(){
         try {
             sqlSession = SqlSessionFactoryUtils.openSqlSession();
             RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             Role role = roleMapper.getRole(1L);
-            User user = userMapper.getUser(1L);
-            System.out.println("role==================");
             log.info(((Role) role).getRoleName());
-            System.out.println("user==================");
-            log.info(user.getSexEnum().getName());
             log.info(role.getNote());
+        }finally {
+            if (sqlSession != null){
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void testUserMapper() {
+        try {
+            sqlSession = SqlSessionFactoryUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.getUser(1L);
+            log.info(user.getSexEnum().getName());
         }finally {
             if (sqlSession != null){
                 sqlSession.close();
