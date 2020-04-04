4f4c79164ddb18e6255ac724d6cf3e6487008f7c
admin-zhang <764018641@qq.com>
2020-04-04 19:19:48 +0800

    ssm+redis框架整合

--- LearnSpring/src/main/java/xyz/codedog/chapter2/profile/ProfileTest.java
+++ LearnSpring/src/main/java/xyz/codedog/chapter2/profile/ProfileTest.java
@@ -1,6 +1,7 @@
 package xyz.codedog.chapter2.profile;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.sql.DataSource;
